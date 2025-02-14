f652d42442b9970783ca945f75484478d28d3b92
sangameshkalashetty <sangameshsangameshkalshetty@gmail.com>
2025-02-14 23:52:22 +0530

    Add files via upload

---
+++ 14 sentiment analysis on restaurant reviews..py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv(r"C:\Users\sangu\Downloads\Restaurant_Reviews.tsv",delimiter='\t',quoting=3)
+
+import re 
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus = []
+
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]', ' ',dataset["Review"][i])
+    review = review.lower()
+    review = review.split()
+    ps=PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]   
+    review = ' '.join(review)
+    corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+X= cv.fit_transform(corpus).toarray()
+
+y=dataset.iloc[:,1].values
+    
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=5)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier=DecisionTreeClassifier()
+classifier.fit(X_train,y_train)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred=classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(cm)
+
+from sklearn.metrics import accuracy_score
+ac = accuracy_score(y_test, y_pred)
+print("Accuracy:", ac)
+
+
+bias=classifier.score(X_train, y_train)
+bias
+
+variance = classifier.score(X_test, y_test)
+print("Testing Accuracy (Variance):", variance)
+
+
